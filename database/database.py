fa50a9340f89028163e7d4a21d575150e01f867a
Armando Rodriguez <arodriguez@negofin.com.py>
2023-11-06 11:07:09 -0300

    Se apunta a base de datos stage

--- database/database.py
+++ database/database.py
@@ -7,7 +7,8 @@ from sqlalchemy import MetaData
 SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:@127.0.0.1:3306/operaciones"
 
 # DB2_DATABASE_URL = "db2:///?Server=10.1.253.100&Port=50000&User=armando&Password=Temporal2023&Database=BASE_NF"
-DB2_DATABASE_URL = "db2+ibm_db://armando:Temporal2023@10.1.253.100:50000/BASE_NF"
+# DB2_DATABASE_URL = "db2+ibm_db://armando:Temporal2023@10.1.253.100:50000/BASE_NF"
+DB2_DATABASE_URL = "db2+ibm_db://armando:Armando$$2o23@10.1.253.161:50000/BASE_SS"
 
 
 metadata = MetaData()
